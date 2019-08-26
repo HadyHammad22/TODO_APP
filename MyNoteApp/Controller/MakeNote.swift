8e9dcb6ae1fc9a8ee8b229209e0144e6c13495b7
hady <hady97hamad@gmail.com>
2019-08-26 09:54:03 +0200

    set navigation bar by edit note

--- MyNoteApp/Controller/MakeNote.swift
+++ MyNoteApp/Controller/MakeNote.swift
@@ -12,10 +12,13 @@ class MakeNote: UIViewController {
     
     @IBOutlet weak var noteTitle: UITextField!
     @IBOutlet weak var noteText: UITextView!
+    @IBOutlet weak var navigationBar: UINavigationBar!
     var editedNote:Note?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         if let note = editedNote{
+            self.navigationBar.topItem?.title = "Edit Note"
             noteTitle.text = note.title
             noteText.text = note.details
         }
